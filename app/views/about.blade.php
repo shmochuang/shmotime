187581e764dbf1a372285b4986bf6c4ccb27d4df
Shmo Chuang <elisajchuang@gmail.com>
2014-09-06 14:46:00 -0400

    about header

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -10,7 +10,7 @@
 
 
 	<!-- Description of app -->
-	<div class="about">
+	<div id="about">
 		<p>
 			This very basic application was created for the DWA15 Summer 2014 course "Dynamic Web Applications" with the Laravel framework for PHP. It was designed as a way to keep track of my babysitting gigs by allowing users (parents) to book "appointments" with me. It does not allow more than one appointment on the same day and does not allow users to create appointments on the day of or in the past. Only upcoming appointments are displayed. The user can also choose to see only his or her own appointments. 
 		</p>
