41584360fa66091f796fd3cae4ac5a425546da00
Shmo Chuang <elisajchuang@gmail.com>
2014-09-06 15:42:22 -0400

    contact

---
+++ app/views/contact.blade.php
@@ -0,0 +1,20 @@
+@extends('_master')
+
+
+@section('title')
+	Contact
+@stop
+
+@section('content')
+
+
+	<!-- Description of app -->
+	<div id="contact">
+		<p>
+			If you have questions, concerns, or comments please contact me at ShmoChuang@gmail.com. Thanks!
+		</p>
+	</div>
+
+
+
+@stop
